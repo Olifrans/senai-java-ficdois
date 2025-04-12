dd6ca0602b4a39944e6654a27da283772b979776
Francisco Oliveira <fransoliveira@gmail.com>
2025-04-12 10:34:53 -0300

    aluno

--- src/main/java/com/escola/senai/model/Aluno.java
+++ src/main/java/com/escola/senai/model/Aluno.java
@@ -1,4 +1,48 @@
 package com.escola.senai.model;
+import jakarta.persistence.*;
 
+
+@Entity
 public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long Id;
+    private String nomeAluno;
+    private String matriculaAluno;
+    private int idadeAluno;
+
+
+    // Getters e Setters da classe aluno
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+
+    public String getMatriculaAluno() {
+        return matriculaAluno;
+    }
+
+    public void setMatriculaAluno(String matriculaAluno) {
+        this.matriculaAluno = matriculaAluno;
+    }
+
+    public int getIdadeAluno() {
+        return idadeAluno;
+    }
+
+    public void setIdadeAluno(int idadeAluno) {
+        this.idadeAluno = idadeAluno;
+    }
 }
